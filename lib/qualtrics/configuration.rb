64419c61d1c69d007774e2f6e799d680f06c6fc3
Luisk Hernandez <luis.macias@koombea.com>
2017-04-05 08:04:04 -0500

    Add logger when adding a participant

--- lib/qualtrics/configuration.rb
+++ lib/qualtrics/configuration.rb
@@ -1,6 +1,6 @@
 module Qualtrics
   class Configuration
-    attr_accessor :version, :user, :token, :library_id, :endpoint, :default_library_id, :organization
+    attr_accessor :version, :user, :token, :library_id, :endpoint, :default_library_id, :organization, :logger
     DEFAULT_VERSION = '2.3'
     DEFAULT_ENDPOINT = 'https://co1.qualtrics.com/WRAPI/ControlPanel/api.php'
 
@@ -8,10 +8,15 @@ module Qualtrics
       block.call(self) if block_given?
       self.version ||= DEFAULT_VERSION
       self.endpoint ||= DEFAULT_ENDPOINT
+      self.logger  ||= begin
+                         logger = Logger.new(STDOUT)
+                         logger.level = Logger::INFO
+                         logger
+                       end
     end
 
-    def update(&block)
-      block.call(self) if block_given?
+      def update(&block)
+        block.call(self) if block_given?
+      end
     end
   end
-end
